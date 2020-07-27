39b89d3526df1c4b64f367c352a7dd28c1307245
vicsmr <vicsmr94@gmail.com>
2020-07-27 18:42:01 +0200

    Add AllTest and PlayerTest

---
+++ src/test/java/usantatecla/tictactoe/AllTest.java
@@ -0,0 +1,14 @@
+package usantatecla.tictactoe;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+    BoardTest.class, 
+    CoordinateTest.class,
+    PlayerTest.class,
+    TurnTest.class } )
+public class AllTest {
+    
+}
---
+++ src/test/java/usantatecla/tictactoe/PlayerTest.java
@@ -0,0 +1,66 @@
+package usantatecla.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PlayerTest {
+    
+    private Board board;
+
+    private Player player;
+
+    private Coordinate coordinate11;
+    private Coordinate coordinate12;
+
+    public PlayerTest() {
+        this.board = new Board();
+        this.player = new UserPlayer(Token.TOKEN_O, this.board);
+        this.coordinate11 = new Coordinate(1, 1);
+        this.coordinate12 = new Coordinate(1, 2);
+    }
+
+    @Test
+    public void testGivenNewPlayerThenPutNewTokenReturnErrorNotOwner() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        assertEquals(Error.NOT_OWNER, this.player.controlErrorsPutCoordinate(this.coordinate11));
+    }
+
+    @Test
+    public void testGivenNewPlayerThenPutNewTokenNotReturnErrorNull() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        assertTrue(this.player.controlErrorsPutCoordinate(this.coordinate12) == null);
+    }
+
+    @Test
+    public void testGivenNewPlayerThenRemoveTokenReturnErrorNotOwner() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        assertEquals(Error.NOT_OWNER, this.player.controlErrorsMoveOriginCoordinate(this.coordinate12));
+    }
+
+    @Test
+    public void testGivenNewPlayerThenRemoveTokenNotReturnErrorNull() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        assertTrue(this.player.controlErrorsMoveOriginCoordinate(this.coordinate11) == null);
+    }
+
+    @Test
+    public void testGivenNewPlayerThenMoveTokenReturnErrorSameCoordinates() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        assertEquals(Error.SAME_COORDINATES, this.player.controlErrorsMoveTargetCoordinate(this.coordinate11, this.coordinate11));
+    }
+
+    @Test
+    public void testGivenNewPlayerThenMoveTokenReturnErrorNotEmpty() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        this.board.put(this.coordinate12, Token.TOKEN_O);
+        assertEquals(Error.NOT_EMPTY, this.player.controlErrorsMoveTargetCoordinate(this.coordinate11, this.coordinate12));
+    }
+
+    @Test
+    public void testGivenNewPlayerThenMoveTokenNotReturnErrorNull() {
+        this.board.put(this.coordinate11, Token.TOKEN_O);
+        assertTrue(this.player.controlErrorsMoveTargetCoordinate(this.coordinate11, this.coordinate12) == null);
+    }
+}
