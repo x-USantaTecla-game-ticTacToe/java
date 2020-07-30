e6d74cef27d60b67775762574cbefc05bba6f6bc
vicsmr <vicsmr94@gmail.com>
2020-07-30 13:10:34 +0200

    Refactor Board views and model

--- src/main/java/usantatecla/tictactoe/views/graphics/BoardView.java
+++ src/main/java/usantatecla/tictactoe/views/graphics/BoardView.java
@@ -17,24 +17,33 @@ class BoardView extends JPanel {
 
     BoardView(Board board) {
         this.setLayout(new GridBagLayout());
-        String boardToPresent = "";
         this.add(new JLabel(MessageView.SEPARATOR.getMessage()), new Constraints(0, 0, 1, 1));
         for (int i = 0; i < Coordinate.DIMENSION; i++) {
-            boardToPresent += MessageView.VERTICAL_LINE_LEFT.getMessage();
-            for (int j = 0; j < Coordinate.DIMENSION; j++) {
-                if (board.getToken(new Coordinate(i, j)) == null) {
-                    boardToPresent += MessageView.EMPTY.getMessage();
-                } else {
-                    Token token = board.getToken(new Coordinate(i, j));
-                    boardToPresent += new TokenView(token).getToken();
-                }
-                boardToPresent += MessageView.VERTICAL_LINE_CENTERED.getMessage();
-            }
-            JLabel label = new JLabel(boardToPresent);
-            label.setHorizontalAlignment(SwingConstants.CENTER);
-            this.add(label, new Constraints(0, i + 1, 10, 1));
-            boardToPresent = "";
+            this.printRowBoard(board, i);
         }
         this.add(new JLabel(MessageView.SEPARATOR.getMessage()), new Constraints(0, 4, 1, 1));
     }
+
+	private void printRowBoard(Board board, int row) {
+        String boardToPresent = "";
+        boardToPresent += MessageView.VERTICAL_LINE_LEFT.getMessage();
+        for (int j = 0; j < Coordinate.DIMENSION; j++) {
+            this.getSquareBoardText(board, row, j);
+        }
+        JLabel label = new JLabel(boardToPresent);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(label, new Constraints(0, row + 1, 10, 1));
+}
+
+	private String getSquareBoardText(Board board, int row, int column) {
+        String squareBoardToPresent = "";
+		if (board.getToken(new Coordinate(row, column)) == null) {
+            squareBoardToPresent += MessageView.EMPTY.getMessage();
+        } else {
+            Token token = board.getToken(new Coordinate(row, column));
+            squareBoardToPresent += new TokenView(token).getToken();
+        }
+        squareBoardToPresent += MessageView.VERTICAL_LINE_CENTERED.getMessage();
+        return squareBoardToPresent;
+	}
 }
