136d550e2996605540adc10b64a1d27b2147665e
vicsmr <vicsmr94@gmail.com>
2020-07-30 12:30:57 +0200

    Refactor Board and BoardView

--- src/main/java/usantatecla/tictactoe/views/BoardView.java
+++ src/main/java/usantatecla/tictactoe/views/BoardView.java
@@ -15,18 +15,26 @@ class BoardView {
     void write() {
 		MessageView.SEPARATOR.writeln();
 		for (int i = 0; i < Coordinate.DIMENSION; i++) {
-            MessageView.VERTICAL_LINE_LEFT.write();
-			for (int j = 0; j < Coordinate.DIMENSION; j++) {
-				if (this.board.getToken(new Coordinate(i, j)) == null) {
-                    MessageView.EMPTY.write();
-				} else {
-                    Token token = this.board.getToken(new Coordinate(i, j));
-                    new TokenView(token).write();
-				}
-                MessageView.VERTICAL_LINE_CENTERED.write();
-			}
-            MessageView.LINE_BREAK.writeln();
+			this.printRowBoard(i);
 		}
 		MessageView.SEPARATOR.writeln();
+	}
+
+	private void printRowBoard(int row) {
+		MessageView.VERTICAL_LINE_LEFT.write();
+		for (int j = 0; j < Coordinate.DIMENSION; j++) {
+			this.printColumnBoard(row, j);
+		}
+		MessageView.LINE_BREAK.writeln();
+	}
+
+	private void printColumnBoard(int row, int column) {
+		Token tokenToWrite = this.board.getToken(new Coordinate(row, column));
+		if (tokenToWrite == null) {
+			MessageView.EMPTY.write();
+		} else {
+			new TokenView(tokenToWrite).write();
+		}
+		MessageView.VERTICAL_LINE_CENTERED.write();
 	}
 }
