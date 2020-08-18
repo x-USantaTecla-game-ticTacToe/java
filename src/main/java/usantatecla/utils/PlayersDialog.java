52cb27dbc12f3eca77186eb6448db10cc62f0e2d
vicsmr <vicsmr94@gmail.com>
2020-08-18 12:58:18 +0200

    Add PlayersDialog's implementation

--- src/main/java/usantatecla/utils/PlayersDialog.java
+++ src/main/java/usantatecla/utils/PlayersDialog.java
@@ -5,7 +5,15 @@ public class PlayersDialog extends WithConsoleView {
 	private static final String USERS_ERROR = "Wrong number of users";
 
 	public int read(int players) {
-		return 2;
+		int users = 0;
+		do {
+			users = this.console.readInt("Number of users [0-" + players + "] ");
+			assert users >= 0 && users < 3;
+			if (users < 0 || users > 2) {
+				this.console.writeln(PlayersDialog.USERS_ERROR);
+			}
+		} while(users < 0 || users > 2);
+		return users;
 	}
     
 }
