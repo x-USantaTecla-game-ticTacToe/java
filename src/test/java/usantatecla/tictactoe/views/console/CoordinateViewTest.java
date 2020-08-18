0957b57cd9b79ec84a4111c152c71563e9f31f7d
vicsmr <vicsmr94@gmail.com>
2020-08-18 14:16:29 +0200

    Add CoordinateViewTest

---
+++ src/test/java/usantatecla/tictactoe/views/console/CoordinateViewTest.java
@@ -0,0 +1,71 @@
+package usantatecla.tictactoe.views.console;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import usantatecla.tictactoe.models.Coordinate;
+import usantatecla.utils.Console;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CoordinateViewTest {
+    static final String ENTER_COORDINATE_TO_PUT = "Enter a coordinate to put a token:";
+
+    @Mock
+    Console console;
+
+    @InjectMocks
+    CoordinateView coordinateView;
+
+    @Test
+    public void testGivenNewCoordinateViewWhenReadCoordinateThenIsCorrect() {
+        when(this.console.readInt("Row: ")).thenReturn(1);
+        when(this.console.readInt("Column: ")).thenReturn(1);
+        int[] coordinateRead = this.coordinateView.read(ENTER_COORDINATE_TO_PUT);
+        Coordinate coordinateExpected = new Coordinate(0, 0);
+        assertEquals(coordinateExpected.getRow(), coordinateRead[0]);
+        assertEquals(coordinateExpected.getColumn(), coordinateRead[1]);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testGivenNewCoordinatesWhenRow4AndColumn4ThenAssertionException() {
+        when(this.console.readInt("Row: ")).thenReturn(4);
+        when(this.console.readInt("Column: ")).thenReturn(4);
+        this.coordinateView.read("Title");
+        verify(this.console).readInt("Row: ");
+        verify(this.console).readInt("Column: ");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testGivenNewCoordinatesWhenRow0AndColumn0ThenAssertionException() {
+        when(this.console.readInt("Row: ")).thenReturn(0);
+        when(this.console.readInt("Column: ")).thenReturn(0);
+        this.coordinateView.read("Title");
+        verify(this.console).readInt("Row: ");
+        verify(this.console).readInt("Column: ");
+    }
+
+    @Test
+    public void testGivenNewCoordinatesWhenRow1AndColumn1ThenIsCorrect() {
+        when(this.console.readInt("Row: ")).thenReturn(1);
+        when(this.console.readInt("Column: ")).thenReturn(1);
+        this.coordinateView.read("Title");
+        verify(this.console).readInt("Row: ");
+        verify(this.console).readInt("Column: ");
+    }
+
+    @Test
+    public void testGivenNewCoordinatesWhenRow3AndColumn3ThenIsCorrect() {
+        when(this.console.readInt("Row: ")).thenReturn(3);
+        when(this.console.readInt("Column: ")).thenReturn(3);
+        this.coordinateView.read("Title");
+        verify(this.console).readInt("Row: ");
+        verify(this.console).readInt("Column: ");
+    }
+}
