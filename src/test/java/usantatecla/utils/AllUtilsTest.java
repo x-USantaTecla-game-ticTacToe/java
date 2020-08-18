7d44cf4ac82bfa0255f77a61b97669aeabc9b001
vicsmr <vicsmr94@gmail.com>
2020-08-18 12:48:40 +0200

    Update AllUtilsTest

--- src/test/java/usantatecla/utils/AllUtilsTest.java
+++ src/test/java/usantatecla/utils/AllUtilsTest.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ 
+@Suite.SuiteClasses({
+    ConsoleTest.class, 
     PlayersDialogTest.class } )
 public class AllUtilsTest {
     
